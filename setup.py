ebe36cdf28234fb1275e8ae0420d33a6b0161273
Warren Usui <warrenusui@earthlink.net>
2018-10-21 03:45:50 -0700

    Increase setup version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="llama_slobber",
-    version="0.0.7",
+    version="0.0.9",
     author="Warren Usui",
     author_email="warrenusui@gmail.com",
     description="Slobr utilities",
